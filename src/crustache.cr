9fcc62f21deebd1f5c86d7c853db1e07cfeade26
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-07-10 14:31:15 +0900

    add utility methods

--- src/crustache.cr
+++ src/crustache.cr
@@ -1,5 +1,24 @@
 require "./crustache/*"
 
 module Crustache
-  # TODO Put your code here
+  OPEN_TAG = "{{".to_slice
+  CLOSE_TAG = "}}".to_slice
+
+  def self.parse(string : String, filename = "<inline>", row = 1)
+    self.parse StringIO.new(string), filename, row
+  end
+
+  def self.parse(io : IO, filename = "<inline>", row = 1)
+    Parser.new(OPEN_TAG, CLOSE_TAG, io, filename, row).parse
+  end
+
+  def self.render(tmpl : Template, ctx : Context, fs = FileSystem.new)
+    String.build do |io|
+      self.render tmpl, ctx, fs, io
+    end
+  end
+
+  def self.render(tmpl : Template, ctx : Context, fs : FileSystem, io : IO)
+    tmpl.visit Renderer.new OPEN_TAG, CLOSE_TAG, [ctx] of Context, fs, io
+  end
 end
