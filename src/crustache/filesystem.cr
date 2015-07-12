8efe0598f820b9d82330cd02d072d742da8e25e4
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-07-12 22:33:31 +0900

    Add ViewLoader

--- src/crustache/filesystem.cr
+++ src/crustache/filesystem.cr
@@ -18,4 +18,33 @@ module Crustache
       return @tmpls[value]?
     end
   end
+
+  class ViewLoader < FileSystem
+    def initialize(@basedir : String, @use_cache = false)
+      @cache = {} of String => Tree::Template?
+    end
+
+    def load(value)
+      if @cache.has_key?(value)
+        return @cache[value]
+      end
+
+      filename = "#{@basedir}/#{value}"
+      filename_ext = "#{filename}.mustache"
+      if File.exists?(filename_ext)
+        tmpl = Crustache.parseFile filename_ext
+        @cache[value] = tmpl if @use_cache
+        return tmpl
+      end
+
+      if File.exists?(filename)
+        tmpl = Crustache.parse filename
+        @cache[value] = tmpl if @use_cache
+        return tmpl
+      end
+
+      @cache[value] = nil if @use_cache
+      return nil
+    end
+  end
 end
