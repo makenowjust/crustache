2d80f1c179d21225809579fe8ebd32eaa90cfd20
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-07-16 18:27:28 +0900

    Add DEFAULT_FILENAME

--- src/crustache.cr
+++ src/crustache.cr
@@ -6,17 +6,19 @@ module Crustache
   # :nodoc:
   CLOSE_TAG = "}}".to_slice
 
+  DEFAULT_FILENAME = "__str__"
+
   alias Template = Syntax::Template+
 
-  def self.parse(io : IO, filename = "<inline>", row = 1)
+  def self.parse(io : IO, filename = DEFAULT_FILENAME, row = 1)
     Parser.new(OPEN_TAG, CLOSE_TAG, io, filename, row).parse
   end
 
-  def self.parse(string : String, filename = "<inline>", row = 1)
+  def self.parse(string : String, filename = DEFAULT_FILENAME, row = 1)
     self.parse StringIO.new(string), filename, row
   end
 
-  def self.parseFile(filename)
+  def self.parse_file(filename)
     self.parse(File.new(filename), filename, 1)
   end
 
@@ -27,6 +29,6 @@ module Crustache
   end
 
   def self.render(tmpl, model, fs, io)
-    tmpl.visit Renderer.new OPEN_TAG, CLOSE_TAG, Crustache::Renderer::Context.new(model), fs, io
+    tmpl.visit Renderer.new OPEN_TAG, CLOSE_TAG, Context.new(model), fs, io
   end
 end
