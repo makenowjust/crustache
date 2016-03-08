f315d32dfb7c3aca50bb3682383b4be761f128bf
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-03-09 08:33:46 +0900

    Remove Util.size

--- src/crustache/util.cr
+++ src/crustache/util.cr
@@ -1,11 +1,3 @@
 # :nodoc:
 module Crustache::Util
-  # Crystal will remove Slice or Array #length (#1363)
-  def self.size(col)
-    if col.responds_to?(:length)
-      col.length
-    else
-      col.size
-    end
-  end
 end
