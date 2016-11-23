0c5041e4baa127654fb203999b883af61e8d76f5
Sijawusz Pur Rahnama <sija@sija.pl>
2016-11-24 00:29:49 +0100

    Update specs

--- spec/generate_spec_from_json.cr
+++ spec/generate_spec_from_json.cr
@@ -7,7 +7,7 @@ def inspect_hash(hash)
     else
       pairs = hash.map do |key, value|
         if key == "lambda"
-          "#{key.inspect} => #{((value as Hash(String, JSON::Type))["ruby"] as String).gsub(/^proc \{ (?:(?:\|)([^|]+)(?:\|))?/){|m, p| "->(#{p[1]? ? "#{p[1]} : String" : ""}){"}.gsub(/1/, "1; $calls.to_s").gsub(/false/, "false.to_s")}"
+          "#{key.inspect} => #{((value.as(Hash(String, JSON::Type)))["ruby"].as String).gsub(/^proc \{ (?:(?:\|)([^|]+)(?:\|))?/){|m, p| "->(#{p[1]? ? "#{p[1]} : String" : ""}){"}.gsub(/\$/, "Global.").gsub(/1/, "1; Global.calls.to_s").gsub(/false/, "false.to_s")}"
         else
           "#{key.inspect} => #{inspect_hash value}"
         end
@@ -19,7 +19,7 @@ def inspect_hash(hash)
     if hash.empty?
       "[] of String"
     else
-      vals = hash.map{|x| inspect_hash(x) as String}
+      vals = hash.map{|x| inspect_hash(x).as String}
       "[#{vals.join ","}]"
     end
   else
@@ -29,11 +29,11 @@ end
 
 filename = ARGV[0]
 
-file = (JSON.parse File.read "./spec/mustache-spec/specs/#{filename}").as_h as Hash(String, JSON::Type)
+file = (JSON.parse File.read "./spec/mustache-spec/specs/#{filename}").as_h.as Hash(String, JSON::Type)
 
 puts "describe #{filename.inspect} do"
-(file["tests"] as Array(JSON::Type)).each do |test|
-  test = test as Hash(String, JSON::Type)
+(file["tests"].as Array(JSON::Type)).each do |test|
+  test = test.as Hash(String, JSON::Type)
 
   puts "    it #{test["desc"].inspect} do"
   puts "      template = Crustache.parse #{test["template"].inspect}"
@@ -43,7 +43,7 @@ puts "describe #{filename.inspect} do"
 
   puts "      fs = Crustache::HashFileSystem.new"
   if test.has_key?("partials")
-    (test["partials"] as Hash(String, JSON::Type)).each do |name, tmpl|
+    (test["partials"].as Hash(String, JSON::Type)).each do |name, tmpl|
       puts "      fs.register #{name.inspect}, Crustache.parse #{tmpl.inspect}"
     end
   end
