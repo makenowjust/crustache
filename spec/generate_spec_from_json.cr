a617a660983daa93aae54a627060b71822b0e10c
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-09-13 16:46:34 +0900

    Fix to run spec correctly

--- spec/generate_spec_from_json.cr
+++ spec/generate_spec_from_json.cr
@@ -1,49 +1,53 @@
 require "json"
 
-def inspect_hash(hash)
-  if hash.is_a?(Hash)
+def inspect_any(any)
+  if hash = any.as_h?
     if hash.empty?
       "{} of String => String"
     else
       pairs = hash.map do |key, value|
         if key == "lambda"
-          "#{key.inspect} => #{((value.as(Hash(String, JSON::Type)))["ruby"].as String).gsub(/^proc \{ (?:(?:\|)([^|]+)(?:\|))?/){|m, p| "->(#{p[1]? ? "#{p[1]} : String" : ""}){"}.gsub(/\$/, "Global.").gsub(/1/, "1; Global.calls.to_s").gsub(/false/, "false.to_s")}"
+          ruby_proc = value.as_h["ruby"].as_s
+          crystal_proc = ruby_proc
+            .gsub(/^proc \{ (?:(?:\|)([^|]+)(?:\|))?/){|m, p| "->(#{p[1]? ? "#{p[1]} : String" : ""}){"}
+            .gsub(/\$/, "Global.").gsub(/1/, "1; Global.calls.to_s").gsub(/false/, "false.to_s")
+          "#{key.inspect} => #{crystal_proc}"
         else
-          "#{key.inspect} => #{inspect_hash value}"
+          "#{key.inspect} => #{inspect_any value}"
         end
       end
 
       "{#{pairs.join ","}}"
     end
-  elsif hash.is_a?(Array)
-    if hash.empty?
+  elsif array = any.as_a?
+    if array.empty?
       "[] of String"
     else
-      vals = hash.map{|x| inspect_hash(x).as String}
+      vals = array.map{|x| inspect_any(x).as(String) }
       "[#{vals.join ","}]"
     end
   else
-    "#{hash.inspect}"
+    "#{any.inspect}"
   end
 end
 
 filename = ARGV[0]
 
-file = (JSON.parse File.read "./spec/mustache-spec/specs/#{filename}").as_h.as Hash(String, JSON::Type)
+file = (JSON.parse File.read "./spec/mustache-spec/specs/#{filename}").as_h
 
 puts "describe #{filename.inspect} do"
-(file["tests"].as Array(JSON::Type)).each do |test|
-  test = test.as Hash(String, JSON::Type)
+file["tests"].as_a.each do |test|
+  test = test.as_h
 
   puts "    it #{test["desc"].inspect} do"
   puts "      template = Crustache.parse #{test["template"].inspect}"
   puts "      expected = #{test["expected"].inspect}"
 
-  puts "      data = #{inspect_hash test["data"]}"
+  puts "      data = #{inspect_any test["data"]}"
 
   puts "      fs = Crustache::HashFileSystem.new"
   if test.has_key?("partials")
-    (test["partials"].as Hash(String, JSON::Type)).each do |name, tmpl|
+    test["partials"].as_h.each do |name, tmpl|
       puts "      fs.register #{name.inspect}, Crustache.parse #{tmpl.inspect}"
     end
   end
